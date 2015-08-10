636885fe349af8ef3ae433ddb38d122ce2b57196
Gabriel Lebec <glebec@gmail.com>
2015-08-10 09:02:40 -0400

    exporting problems and running args

--- exercise-one.js
+++ exercise-one.js
@@ -1,137 +1,166 @@
 'use strict';
 
-var Promise = require('bluebird');
-var exerciseUtils = require('./utils');
+var Promise = require('bluebird'),
+    exerciseUtils = require('./utils');
 
-var readFile = exerciseUtils.readFile;
-var promisifiedReadFile = exerciseUtils.promisifiedReadFile;
+var readFile = exerciseUtils.readFile,
+    promisifiedReadFile = exerciseUtils.promisifiedReadFile,
+    green = exerciseUtils.green,
+    red = exerciseUtils.red;
 
-var green = exerciseUtils.green;
-var red = exerciseUtils.red;
+var args = process.argv.slice(2).map(function(st){ return st.toUpperCase(); });
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- * A. log poem one stanza one (ignore errors)
- *
- */
+module.exports = {
+  problemA: problemA,
+  problemB: problemB,
+  problemC: problemC,
+  problemD: problemD,
+  problemE: problemE,
+  problemF: problemF
+};
 
-// callback version
-readFile('poem-one/stanza-01.txt', function (err, stanza) {
-	console.log('-- A. callback version --');
-	green(stanza);
+// runs every problem given as command-line argument to process
+args.forEach(function(arg){
+  module.exports['problem' + arg]();
 });
 
-// promise version
-// ???
+function problemA () {
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   *
+   * A. log poem one stanza one (ignore errors)
+   *
+   */
 
+  // callback version
+  readFile('poem-one/stanza-01.txt', function (err, stanza) {
+    console.log('-- A. callback version --');
+    green(stanza);
+  });
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- * B. log poem one stanza two and three, in any order
- *    (ignore errors)
- *
- */
+  // promise version
+  // ???
 
-// callback version
-readFile('poem-one/stanza-02.txt', function (err, stanza2) {
-	console.log('-- B. callback version (stanza two) --');
-	green(stanza2);
-});
-readFile('poem-one/stanza-03.txt', function (err, stanza3) {
-	console.log('-- B. callback version (stanza three) --');
-	green(stanza3);
-});
+}
 
-// promise version
-// ???
+function problemB () {
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   *
+   * B. log poem one stanza two and three, in any order
+   *    (ignore errors)
+   *
+   */
 
+  // callback version
+  readFile('poem-one/stanza-02.txt', function (err, stanza2) {
+    console.log('-- B. callback version (stanza two) --');
+    green(stanza2);
+  });
+  readFile('poem-one/stanza-03.txt', function (err, stanza3) {
+    console.log('-- B. callback version (stanza three) --');
+    green(stanza3);
+  });
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- * C. read & log poem one stanza two and *then* read & log stanza three
- *    log 'done' when both are done
- *    (ignore errors)
- *
- */
+  // promise version
+  // ???
 
-// callback version
-readFile('poem-one/stanza-02.txt', function (err, stanza2) {
-	console.log('-- C. callback version (stanza two) --');
-	green(stanza2);
-	readFile('poem-one/stanza-03.txt', function (err, stanza3) {
-		console.log('-- C. callback version (stanza three) --');
-		green(stanza3);
-		console.log('-- C. callback version done --');
-	});
-});
+}
 
-// promise version (hint: don't need to nest `then` calls)
-// ???
+function problemC () {
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   *
+   * C. read & log poem one stanza two and *then* read & log stanza three
+   *    log 'done' when both are done
+   *    (ignore errors)
+   *
+   */
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- * D. log poem one stanza four or an error if it occurs
- *
- */
+  // callback version
+  readFile('poem-one/stanza-02.txt', function (err, stanza2) {
+    console.log('-- C. callback version (stanza two) --');
+    green(stanza2);
+    readFile('poem-one/stanza-03.txt', function (err, stanza3) {
+      console.log('-- C. callback version (stanza three) --');
+      green(stanza3);
+      console.log('-- C. callback version done --');
+    });
+  });
 
-// callback version
-readFile('poem-one/wrong-file-name.txt', function (err, stanza4) {
-	console.log('-- D. callback version (stanza four) --');
-	if (err) red(err);
-	else green(stanza4);
-});
+  // promise version (hint: don't need to nest `then` calls)
+  // ???
 
-// promise version
-// ???
+}
 
+function problemD () {
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   *
+   * D. log poem one stanza four or an error if it occurs
+   *
+   */
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- * E. read & log poem one stanza three and *then* read & log stanza four
- *    or log an error if it occurs for either file read
- *
- */
+  // callback version
+  readFile('poem-one/wrong-file-name.txt', function (err, stanza4) {
+    console.log('-- D. callback version (stanza four) --');
+    if (err) red(err);
+    else green(stanza4);
+  });
 
-// callback version
-readFile('poem-one/stanza-03.txt', function (err, stanza3) {
-	console.log('-- E. callback version (stanza three) --');
-	if (err) return red(err);
-	green(stanza3);
-	readFile('poem-one/wrong-file-name.txt', function (err2, stanza4) {
-		console.log('-- E. callback version (stanza four) --');
-		if (err2) return red(err2);
-		green(stanza4);
-	});
-});
+  // promise version
+  // ???
 
-// promise version
-// ???
+}
 
+function problemE () {
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   *
+   * E. read & log poem one stanza three and *then* read & log stanza four
+   *    or log an error if it occurs for either file read
+   *
+   */
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- * F. read & log poem one stanza three and *then* read & log stanza four
- *    or log an error if it occurs for either file read
- *    always log 'done' when everything is done
- *
- */
+  // callback version
+  readFile('poem-one/stanza-03.txt', function (err, stanza3) {
+    console.log('-- E. callback version (stanza three) --');
+    if (err) return red(err);
+    green(stanza3);
+    readFile('poem-one/wrong-file-name.txt', function (err2, stanza4) {
+      console.log('-- E. callback version (stanza four) --');
+      if (err2) return red(err2);
+      green(stanza4);
+    });
+  });
 
-// callback version
-readFile('poem-one/stanza-03.txt', function (err, stanza3) {
-	console.log('-- F. callback version (stanza three) --');
-	if (err) {
-		red(err);
-		console.log('-- F. callback version done --');
-		return;
-	}
-	green(stanza3);
-	readFile('poem-one/wrong-file-name.txt', function (err2, stanza4) {
-		console.log('-- F. callback version (stanza four) --');
-		if (err2) red(err2);
-		else green(stanza4);
-		console.log('-- F. callback version done --');
-	});
-});
+  // promise version
+  // ???
 
-// promise version
-// ???
+}
+
+function problemF () {
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   *
+   * F. read & log poem one stanza three and *then* read & log stanza four
+   *    or log an error if it occurs for either file read
+   *    always log 'done' when everything is done
+   *
+   */
+
+  // callback version
+  readFile('poem-one/stanza-03.txt', function (err, stanza3) {
+    console.log('-- F. callback version (stanza three) --');
+    if (err) {
+      red(err);
+      console.log('-- F. callback version done --');
+      return;
+    }
+    green(stanza3);
+    readFile('poem-one/wrong-file-name.txt', function (err2, stanza4) {
+      console.log('-- F. callback version (stanza four) --');
+      if (err2) red(err2);
+      else green(stanza4);
+      console.log('-- F. callback version done --');
+    });
+  });
+
+  // promise version
+  // ???
+
+}
--- exercise-two.js
+++ exercise-two.js
@@ -1,134 +1,170 @@
 'use strict';
 
-var Promise = require('bluebird');
-var async = require('async');
-var exerciseUtils = require('./utils');
+var Promise = require('bluebird'),
+    async = require('async'),
+    exerciseUtils = require('./utils');
 
-var readFile = exerciseUtils.readFile;
-var promisifiedReadFile = exerciseUtils.promisifiedReadFile;
+var readFile = exerciseUtils.readFile,
+    promisifiedReadFile = exerciseUtils.promisifiedReadFile,
+    green = exerciseUtils.green,
+    red = exerciseUtils.red;
 
-var green = exerciseUtils.green;
-var red = exerciseUtils.red;
+var args = process.argv.slice(2).map(function(st){ return st.toUpperCase(); });
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- * A. log poem two stanza one and stanza two, in any order
- *    but log 'done' when both are done
- *    (ignore errors)
- *    note: reads are occurring in parallel (simultaneously)
- *
- */
+module.exports = {
+  problemA: problemA,
+  problemB: problemB,
+  problemC: problemC,
+  problemD: problemD,
+  problemE: problemE
+};
 
-// callback version
-async.each(['poem-two/stanza-01.txt', 'poem-two/stanza-02.txt'],
-	function (filename, eachDone) {
-		readFile(filename, function (err, stanza) {
-			console.log('-- A. callback version --');
-			green(stanza);
-			eachDone();
-		});
-	},
-	function (err) {
-		console.log('-- A. callback version done --');
-	}
-);
-
-// promise version
-// ???
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- * B. log all the stanzas in poem two, in any order
- *    and log 'done' when they're all done
- *    (ignore errors)
- *    note: reads are occurring in parallel (simultaneously)
- *
- */
-
-var filenames = [1,2,3,4,5,6,7,8].map(function (n) {
-	return 'poem-two/' + 'stanza-0' + n + '.txt';
+// runs every problem given as command-line argument to process
+args.forEach(function(arg){
+  module.exports['problem' + arg]();
 });
 
-// callback version
-async.each(filenames,
-	function (filename, eachDone) {
-		readFile(filename, function (err, stanza) {
-			console.log('-- B. callback version --');
-			green(stanza);
-			eachDone();
-		});
-	},
-	function (err) {
-		console.log('-- B. callback version done --');
-	}
-);
+function problemA () {
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   *
+   * A. log poem two stanza one and stanza two, in any order
+   *    but log 'done' when both are done
+   *    (ignore errors)
+   *    note: reads are occurring in parallel (simultaneously)
+   *
+   */
 
-// promise version
-// ???
+  // callback version
+  async.each(['poem-two/stanza-01.txt', 'poem-two/stanza-02.txt'],
+    function (filename, eachDone) {
+      readFile(filename, function (err, stanza) {
+        console.log('-- A. callback version --');
+        green(stanza);
+        eachDone();
+      });
+    },
+    function (err) {
+      console.log('-- A. callback version done --');
+    }
+  );
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- * C. read & log all the stanzas in poem two, *in order*
- *    and log 'done' when they're all done
- *    (ignore errors)
- *    note: reads are occurring in series (only when previous finishes)
- *
- */
+  // promise version
+  // ???
 
-// callback version
-async.eachSeries(filenames,
-	function (filename, eachDone) {
-		readFile(filename, function (err, stanza) {
-			console.log('-- C. callback version --');
-			green(stanza);
-			eachDone();
-		});
-	},
-	function (err) {
-		console.log('-- C. callback version done --');
-	}
-);
+}
 
-// promise version
-// ???
+function problemB () {
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   *
+   * B. log all the stanzas in poem two, in any order
+   *    and log 'done' when they're all done
+   *    (ignore errors)
+   *    note: reads are occurring in parallel (simultaneously)
+   *
+   */
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- * D. log all the stanzas in poem two, *in order*
- *    making sure to fail for any error and log it out
- *    and log 'done' when they're all done
- *    note: reads are occurring in series (only when previous finishes)
- *
- */
+  var filenames = [1, 2, 3, 4, 5, 6, 7, 8].map(function (n) {
+    return 'poem-two/' + 'stanza-0' + n + '.txt';
+  });
 
-// COMMENT OUT THIS CODE FOR ALL EXERCISES ABOVE
-var randIdx = Math.floor(Math.random() * filenames.length);
-filenames[randIdx] = 'wrong-file-name-' + randIdx + '.txt';
+  // callback version
+  async.each(filenames,
+    function (filename, eachDone) {
+      readFile(filename, function (err, stanza) {
+        console.log('-- B. callback version --');
+        green(stanza);
+        eachDone();
+      });
+    },
+    function (err) {
+      console.log('-- B. callback version done --');
+    }
+  );
 
-// callback version
-async.eachSeries(filenames,
-	function (filename, eachDone) {
-		readFile(filename, function (err, stanza) {
-			console.log('-- D. callback version --');
-			if (err) return eachDone(err);
-			green(stanza);
-			eachDone();
-		});
-	},
-	function (err) {
-		if (err) red(err);
-		console.log('-- D. callback version done --');
-	}
-);
+  // promise version
+  // ???
 
-// promise version
-// ???
+}
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- * E. make a promisifed version of fs.writeFile
- *
- */
+function problemC () {
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   *
+   * C. read & log all the stanzas in poem two, *in order*
+   *    and log 'done' when they're all done
+   *    (ignore errors)
+   *    note: reads are occurring in series (only when previous finishes)
+   *
+   */
 
-var fs = require('fs');
-function promisifiedWriteFile (filename, str) {}
+  var filenames = [1, 2, 3, 4, 5, 6, 7, 8].map(function (n) {
+    return 'poem-two/' + 'stanza-0' + n + '.txt';
+  });
+
+  // callback version
+  async.eachSeries(filenames,
+    function (filename, eachDone) {
+      readFile(filename, function (err, stanza) {
+        console.log('-- C. callback version --');
+        green(stanza);
+        eachDone();
+      });
+    },
+    function (err) {
+      console.log('-- C. callback version done --');
+    }
+  );
+
+  // promise version
+  // ???
+
+}
+
+function problemD () {
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   *
+   * D. log all the stanzas in poem two, *in order*
+   *    making sure to fail for any error and log it out
+   *    and log 'done' when they're all done
+   *    note: reads are occurring in series (only when previous finishes)
+   *
+   */
+
+  var filenames = [1, 2, 3, 4, 5, 6, 7, 8].map(function (n) {
+    return 'poem-two/' + 'stanza-0' + n + '.txt';
+  });
+  var randIdx = Math.floor(Math.random() * filenames.length);
+  filenames[randIdx] = 'wrong-file-name-' + randIdx + '.txt';
+
+  // callback version
+  async.eachSeries(filenames,
+    function (filename, eachDone) {
+      readFile(filename, function (err, stanza) {
+        console.log('-- D. callback version --');
+        if (err) return eachDone(err);
+        green(stanza);
+        eachDone();
+      });
+    },
+    function (err) {
+      if (err) red(err);
+      console.log('-- D. callback version done --');
+    }
+  );
+
+  // promise version
+  // ???
+
+}
+
+function problemE () {
+  /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+   *
+   * E. make a promisifed version of fs.writeFile
+   *
+   */
+
+  var fs = require('fs');
+  function promisifiedWriteFile (filename, str) {
+    // your code here
+  }
+}
